1be40fb250fa4c4788d4ee1bed6dcf6ac33d1680
Diogo Alves <diogodidi2022@gmail.com>
2022-12-01 01:13:14 -0300

    adding effects on the technology page

--- src/components/pages/technology/index.tsx
+++ src/components/pages/technology/index.tsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
+import gsap from "gsap";
 
 import "@glidejs/glide/src/assets/sass/glide.core.scss";
 import Glide from "@glidejs/glide";
 
 import { technology } from '../../../service/data.json';
-import { getImageByIndex, imagesRocketsDesktop } from "../../../utils/GetIndexImages";
+import {
+    getImageByIndex,
+    imagesRocketsDesktop,
+    RocketsSmallerDevices
+} from "../../../utils/GetIndexImages";
 
-import { 
+import {
     BgImage,
     WrapperTecnology,
     Container,
@@ -18,33 +23,32 @@ import {
 const Technology = (props: any) => {
 
     const [showMenu, setShowMenu] = useState(true);
-    
+
     useEffect(() => {
-        const glide = new Glide("#glide",{
+        const glide = new Glide("#glide", {
             type: 'slider',
             perView: 1
         })
 
-        glide.mount()
+        glide.mount();
+
     }, [])
 
+    useEffect(() => {
+        const tl = gsap.timeline();
+
+        tl.fromTo("#bg", { opacity: 0 }, { opacity: 1, duration: 3 }, 1);
+
+    }, []);
 
     const getWidth = (): number => window.innerWidth
         || document.documentElement.clientWidth
         || document.body.clientWidth;
 
-
-
     function useCurrentWidth(): number {
-
         const [width, setWidth] = useState(getWidth());
 
         useEffect(() => {
-            const tl = gsap.timeline();
-
-            // tl.fromTo("#logo", {y: 1500, opacity: 0}, {y: 0, opacity: 1}, 3)
-            // tl.fromTo("#line", {x: 1500, opacity: 0}, {x: 0, opacity: 1}, 3.1)
-            // tl.fromTo("#navbar", {y: -1500, opacity: 0}, {y: 0, opacity: 1}, 3.5)
 
             let timeoutId: any;
 
@@ -61,15 +65,16 @@ const Technology = (props: any) => {
                 window.removeEventListener('resize', resizeListener);
             }
 
-
         }, [])
 
         return width;
     }
 
+    const width: number = useCurrentWidth();
+
     return (
         <>
-            <BgImage />
+            <BgImage id="bg" />
             {
                 technology ? (
                     <div id="glide">
@@ -78,18 +83,20 @@ const Technology = (props: any) => {
                                 {technology.map((item, index) =>
                                     <li key={index}>
                                         <WrapperTecnology>
-                                            <Container>                                         
-                                                    <NavigationPoints data-glide-el="controls[nav]">
-                                                        <span data-glide-dir="=0">1</span>
-                                                        <span data-glide-dir="=1">2</span>
-                                                        <span data-glide-dir="=2">3</span>                                                                                   
-                                                    </NavigationPoints>
-                                                    <Informations>
-                                                        <h1>{item.name}</h1>
-                                                        <p>{item.description}</p>
-                                                    </Informations>                                              
+                                            <Container>
+                                                <NavigationPoints data-glide-el="controls[nav]">
+                                                    <span data-glide-dir="=0">1</span>
+                                                    <span data-glide-dir="=1">2</span>
+                                                    <span data-glide-dir="=2">3</span>
+                                                </NavigationPoints>
+                                                <Informations>
+                                                    <h1>{item.name}</h1>
+                                                    <p>{item.description}</p>
+                                                </Informations>
                                                 <ContainerImage>
-                                                    <img src={getImageByIndex<string>(index, imagesRocketsDesktop)} alt="" />
+                                                    <img src={ width < 1000
+                                                        ? getImageByIndex<string>(index, RocketsSmallerDevices)
+                                                        : getImageByIndex<string>(index, imagesRocketsDesktop)} alt={item.name} />
                                                 </ContainerImage>
                                             </Container>
                                         </WrapperTecnology>
