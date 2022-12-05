32d4c3dc3985f1a49faeca76e7f6cda8115c5b7d
Diogo Alves <diogodidi2022@gmail.com>
2022-12-05 19:08:35 -0300

    add dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: "/space-tourism-website-frontend-mentor/"
 })
 
 
