f0b56b77fcc0c5b6bb1e85a380dddb275413a174
Diogo Alves <diogodidi2022@gmail.com>
2022-12-06 00:28:37 -0300

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/space-tourism-website-frontend-mentor",
   plugins: [react()]
 })
 
